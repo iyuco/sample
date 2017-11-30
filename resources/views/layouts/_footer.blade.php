27adfe518ada6c13c8179e2c6055ebda86e1b675
iyuco <sylonamon@gmail.com>
2017-11-30 04:24:39 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,14 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+        &copy;&nbsp;<a href="http://www.gemshi.com">
+        Gemshi
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
