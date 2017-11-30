77957683fdf51afaff345aaeeba876a54c2398df
iyuco <sylonamon@gmail.com>
2017-11-30 08:49:44 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    //注册页面控制器：用户名、邮箱、密码
+    public function store(Request $request)
+    {
+        //验证
+        $this->validate($request, [
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        //验证通过后，添加到数据库
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+
+        //创建成功后的路由，及欢迎信息
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,19 @@
 |
 */
 
+// Static Pages
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+//Users
 Route::get('signup','UsersController@create')->name('signup');
+Route::resource('users','UsersController');
+//上面一行代码相当于下面的几行代码，使用resource让代码非常精简：
+// Route::get('/users', 'UsersController@index')->name('users.index');
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// Route::get('/users/create', 'UsersController@create')->name('users.create');
+// Route::post('/users', 'UsersController@store')->name('users.store');
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+// Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
